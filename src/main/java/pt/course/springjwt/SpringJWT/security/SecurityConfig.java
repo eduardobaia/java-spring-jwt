7a23b9ac4a647ce343e1664bb4dded034903ea3c
Eduardo Baia <eduardobaia@Eduardos-Air.Home>
2023-07-02 15:12:09 +0100

    add filter and authentitacion manager bean

--- src/main/java/pt/course/springjwt/SpringJWT/security/SecurityConfig.java
+++ src/main/java/pt/course/springjwt/SpringJWT/security/SecurityConfig.java
@@ -1,13 +1,17 @@
-package pt.course.springjwt.SpringJWT.service;
+package pt.course.springjwt.SpringJWT.security;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import pt.course.springjwt.SpringJWT.filter.CustomAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -26,6 +30,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http)throws  Exception{
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests().anyRequest().permitAll();
+        http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()))
+    }
 
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception{
+        //gets the authentication manager bean from the class pather.
+        // and pass it to my filter CustiomAuthenticationFilter
+        //cause this class have extends WebSecurityConfigurerAdapter and it has inside one bnean of authentication bean
+
+        return super.authenticationManagerBean();
     }
 }
