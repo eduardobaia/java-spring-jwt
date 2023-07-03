214a5cb0b959373c0a046b657c6576aa30ae1792
Eduardo Baia <eduardobaia@Eduardos-Air.Home>
2023-07-03 20:09:41 +0100

    login working

--- src/main/java/pt/course/springjwt/SpringJWT/service/UserServiceImpl.java
+++ src/main/java/pt/course/springjwt/SpringJWT/service/UserServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import pt.course.springjwt.SpringJWT.domain.Role;
@@ -25,10 +26,12 @@ public class UserServiceImpl implements UserService, UserDetailsService {
 
     private final UserRepository userRepository;
     private final RoleRepository roleRespository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public User saveUser(User user) {
         log.info("Saving user");
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
